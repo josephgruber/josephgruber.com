71749a510a5101e22b7ab564eff2b56e8b274aa8
Joseph Gruber <joseph.gruber@gmail.com>
2023-01-30 14:02:58 -0700

    Standardize S3 infrastructure

--- tf/s3.tf
+++ tf/s3.tf
@@ -1,40 +1,18 @@
-resource "aws_s3_bucket" "main" {
+resource "aws_s3_bucket" "main" { #tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-encryption
   bucket = var.domain
-  acl    = "private"
-  policy = templatefile("templates/s3-cf-oai-policy.json", {
-    oai_arn = "${aws_cloudfront_origin_access_identity.oai.iam_arn}"
-    bucket  = "${var.domain}"
-    }
-  )
-  force_destroy = false
-
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
 }
 
 resource "aws_s3_bucket" "email" {
   bucket = "${var.domain}-email"
-  acl    = "private"
-  policy = templatefile("templates/s3-ses-policy.json", {
-    bucket     = "${var.domain}-email",
-    account_id = data.aws_caller_identity.account.account_id
-    }
-  )
-  force_destroy = false
+}
 
-  lifecycle_rule {
-    id                                     = "Delete Old Emails"
-    enabled                                = true
-    abort_incomplete_multipart_upload_days = 0
-    prefix                                 = "incoming/"
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
 
-    expiration {
-      days                         = 7
-      expired_object_delete_marker = false
-    }
-  }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_public_access_block" "email" {
@@ -44,4 +22,38 @@ resource "aws_s3_bucket_public_access_block" "email" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "cloudfront_policy" {
+  bucket = aws_s3_bucket.main.id
+  policy = templatefile("s3-cf-oac-policy.tftpl", {
+    bucket_name  = aws_s3_bucket.main.id,
+    account      = data.aws_caller_identity.account.account_id,
+    distribution = aws_cloudfront_distribution.distribution.id
+  })
+}
+
+resource "aws_s3_bucket_policy" "ses_policy" {
+  bucket = aws_s3_bucket.email.id
+  policy = templatefile("templates/s3-ses-policy.json", {
+    bucket     = aws_s3_bucket.email.bucket
+    account_id = data.aws_caller_identity.account.account_id
+    }
+  )
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "email_lifecycle" {
+  bucket = aws_s3_bucket.email.id
+
+  rule {
+    id                                     = "Delete Old Emails"
+    status                                 = "Enabled"
+    abort_incomplete_multipart_upload_days = 0
+    prefix                                 = "incoming/"
+
+    expiration {
+      days                         = 7
+      expired_object_delete_marker = false
+    }
+  }
 }
